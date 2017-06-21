a6096b25589015d64edeacca100523463acb1d86
Jonathan Sebastian <jojosebseb@gmail.com>
2017-06-21 13:22:50 +0700

    v1

---
+++ script/jquery.js
@@ -0,0 +1,64 @@
+var scrollTop, scrollValueSm;
+
+
+
+$(window).scroll(function(){
+    scrollTop = $(document).scrollTop();
+    scrollValueSm= (scrollTop / 2)-30;
+    scrollValueSl= (scrollTop / 2)+380;
+    if ($(window).width() > 600) {
+        $('#indexBanner2').css({
+            backgroundPosition: 'center '+ scrollValueSl + 'px'
+        })
+        $('#indexBanner3').css({
+            backgroundPosition: 'center '+ scrollValueSm + 'px'
+        })
+    }
+
+
+
+
+});
+
+$('#indexSlider > .slide-parent').slick({
+    fade: true,
+    arrows: false,
+    autoplay: true,
+    infinite: true,
+    autolaySpeed: 2000
+})
+
+$('#destinationBanner').waypoint(function(){
+    console.log('asd');
+    $('.choice-module').addClass('active');
+}, {offset: '70%'})
+
+$('.mobile-handle').on('click',function(){
+    $(this).toggleClass('active');
+    $('.nav-ul').toggleClass('active');
+})
+
+$('.register-handle').on('click', function(){
+    $('#registerPop').addClass('active');
+})
+
+$('.login-handle').on('click', function(){
+    $('#loginPop').addClass('active');
+})
+
+$('.popup-fader').on('click', function(){
+    $(this).parent().removeClass('active');
+})
+
+
+$('<div class="jo-checkbox-mask"></div>').insertAfter('.jo-checkbox');
+
+$('.jo-checkbox').on('click', function(){
+    $(this).parent().find('.jo-checkbox-mask').toggleClass('active');
+})
+
+$('.tips-handle').on('click', function(){
+
+    $('.tips-handle').removeClass('active');
+    $(this).addClass('active');
+})
